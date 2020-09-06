6b80a80b993af6629cf79e04473cc2cf01962d4a
wuguang00 <wuguang20084520@163.com>
2020-09-07 06:55:38 +0800

    Added the case about Kbuild.

---
+++ s06_kbuild/myprint.h
@@ -0,0 +1,10 @@
+/**************************************************************************
+ File Name:     myprint.h
+ Author:        wuguang
+ E-mail:        wuguang20084520@163.com
+ Created Time:  Tue Aug 18 00:08:22 2020
+ Release Notes: 
+ **************************************************************************/
+#ifndef MYPRINT_H_
+#define MYPRINT_H_
+#endif
